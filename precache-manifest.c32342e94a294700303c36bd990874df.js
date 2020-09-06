04512c295c89c846ef0789f953b5eb39d15bd49f
EshwarCVS <thedla.ecvs@gmail.com>
2020-09-06 14:43:10 +0530

    Updates

---
+++ precache-manifest.c32342e94a294700303c36bd990874df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15efa74703e02d7b436ff37be2456e3",
+    "url": "/messenger/index.html"
+  },
+  {
+    "revision": "65060ca928e06e4d5ea5",
+    "url": "/messenger/static/css/main.7c04f86a.chunk.css"
+  },
+  {
+    "revision": "0e8ac8b661c525256b76",
+    "url": "/messenger/static/js/2.9221975b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/messenger/static/js/2.9221975b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65060ca928e06e4d5ea5",
+    "url": "/messenger/static/js/main.e6bf2896.chunk.js"
+  },
+  {
+    "revision": "42b02ae745a4878b278e",
+    "url": "/messenger/static/js/runtime-main.68fb1750.js"
+  }
+]);
